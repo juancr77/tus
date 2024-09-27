b8006f93934a60b1aa79247ce307eaf2a01371a7
Juan cr <cruz_j@uadec.edu.mx>
2024-09-27 15:17:18 -0600

    gre

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import SocialIcons from './components/SocialIcons';
 function App() {
   return (
     <div className="App">
-      {/* <h1>Contenido de la página</h1> */}
+
       <SocialIcons />
     </div>
   );
