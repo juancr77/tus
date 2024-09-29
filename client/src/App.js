b04813fa78624e9d81087319d527c24dc524b548
Juan cr <cruz_j@uadec.edu.mx>
2024-09-28 20:24:49 -0600

    holo

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
 
       <SocialIcons />
+
     </div>
+    
   );
 }
 
--- client/src/components/SocialIcons.js
+++ client/src/components/SocialIcons.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './SocialIcons.css';
-import { FaInstagram, FaFacebook } from 'react-icons/fa';
+import { FaInstagram, FaFacebook, FaWhatsapp } from 'react-icons/fa'; // Importamos el ícono de WhatsApp
 
 function SocialIcons() {
   return (
-    <div className="social-icons">
-      <a href="https://www.instagram.com/tus_uadec" target="_blank" rel="noopener noreferrer">
-        <FaInstagram className="icon instagram" />
-      </a>
-      <a href="https://www.facebook.com/TUSUAdeC" target="_blank" rel="noopener noreferrer">
-        <FaFacebook className="icon facebook" />
+    <div className="social-icons-container">
+      <div className="social-icons">
+        <a href="https://www.instagram.com/tus_uadec" target="_blank" rel="noopener noreferrer">
+          <FaInstagram className="icon instagram" />
+        </a>
+        <a href="https://www.facebook.com/TUSUAdeC" target="_blank" rel="noopener noreferrer">
+          <FaFacebook className="icon facebook" />
+        </a>
+      </div>
+
+      {/* Botón de contacto con WhatsApp */}
+      <a href="https://wa.me/528611023793" target="_blank" rel="noopener noreferrer" className="whatsapp-button">
+        <FaWhatsapp className="whatsapp-icon" /> Contáctanos
       </a>
     </div>
   );
